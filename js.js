39376b1a048b0ea7599aef1fe672aae60a5cbfb9
Jerwin Cabral <jerwincabral94@gmail.com>
2020-07-08 20:30:21 +0800

    add js code sample

--- js.js
+++ js.js
@@ -42,8 +42,9 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject("Data not received");
   }
 });
-
 makeServerRequest.then(result=>{
 console.log(result);
 });
-  
+makeServerRequest.catch(error=> {
+  console.log(error);
+  });
