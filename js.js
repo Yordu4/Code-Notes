189c6040ac4fe4485093bc11c841cd53826aa942
Jerwin Cabral <jerwincabral94@gmail.com>
2020-07-08 10:37:54 +0800

    update notes

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {today,tomorrow} = HIGH_TEMPERATURES;
+  console.log(today);
+  // Only change code above this line
+  
